10814a35b84a6e778e13606ea9ef16bc668c721d
Eric Crain <ericcrain77@gmail.com>
2021-12-18 21:18:09 -0500

    adding js

---
+++ develop/assets/js/script.js
@@ -0,0 +1,43 @@
+// setting variables
+var pastSearches = document.querySelector('#past-searches');
+var cityNameEl = document.querySelector('#results-city');
+var currentTemp = document.querySelector('#current-temp');
+var currentWind = document.querySelector('#current-wind');
+var currentHum = document.querySelector('#current-hum');
+var currentUv = document.querySelector('#current-uv');
+var forecastDay1 = document.querySelector('#day-0');
+var day1Temp = document.querySelector('#temp-0');
+var day1Wind = document.querySelector('#wind-0');
+var day1Hum = document.querySelector('#hum-0');
+var forecastDay2 = document.querySelector('#day-1');
+var day2Temp = document.querySelector('#temp-1');
+var day2Wind = document.querySelector('#wind-1');
+var day2Hum = document.querySelector('#hum-1');
+var forecastDay3 = document.querySelector('#day-2');
+var day3Temp = document.querySelector('#temp-2');
+var day3Wind = document.querySelector('#wind-2');
+var day3Hum = document.querySelector('#hum-2');
+var forecastDay4 = document.querySelector('#day-3');
+var day4Temp = document.querySelector('#temp-3');
+var day4Wind = document.querySelector('#wind-3');
+var day4Hum = document.querySelector('#hum-3');
+var forecastDay5 = document.querySelector('#day-4');
+var day5Temp = document.querySelector('#temp-4');
+var day5Wind = document.querySelector('#wind-4');
+var day5Hum = document.querySelector('#hum-4');
+
+
+// retreiving weather data for city
+function retreiveWeather(lat, lon, city) {
+    var api = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=a0261386ac4ee516e814165ba45ab66c';
+
+    fetch(api).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+
+            });
+        } else {
+            console.log(err)
+        }
+    })
+}
