d94ee3191b1eacbce3cfc553a04cf88000654529
Eric Crain <ericcrain77@gmail.com>
2021-12-23 15:21:23 -0500

    working on stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
 // setting variables
-var pastSearches = document.querySelector('#past-searches');
-var cityNameEl = document.querySelector('#results-city');
-var currentTemp = document.querySelector('#current-temp');
-var currentWind = document.querySelector('#current-wind');
-var currentHum = document.querySelector('#current-hum');
-var currentUv = document.querySelector('#current-uv');
-var forecastDay1 = document.querySelector('#day-0');
-var day1Temp = document.querySelector('#temp-0');
-var day1Wind = document.querySelector('#wind-0');
-var day1Hum = document.querySelector('#hum-0');
-var forecastDay2 = document.querySelector('#day-1');
-var day2Temp = document.querySelector('#temp-1');
-var day2Wind = document.querySelector('#wind-1');
-var day2Hum = document.querySelector('#hum-1');
-var forecastDay3 = document.querySelector('#day-2');
-var day3Temp = document.querySelector('#temp-2');
-var day3Wind = document.querySelector('#wind-2');
-var day3Hum = document.querySelector('#hum-2');
-var forecastDay4 = document.querySelector('#day-3');
-var day4Temp = document.querySelector('#temp-3');
-var day4Wind = document.querySelector('#wind-3');
-var day4Hum = document.querySelector('#hum-3');
-var forecastDay5 = document.querySelector('#day-4');
-var day5Temp = document.querySelector('#temp-4');
-var day5Wind = document.querySelector('#wind-4');
-var day5Hum = document.querySelector('#hum-4');
+var pastSearches = document.getElementById('#past-searches');
+var cityNameEl = document.getElementById('#results-city');
+var currentDate = document.querySelector('#current-date');
+var currentTemp = document.getElementById('#current-temp');
+var currentWind = document.getElementById('#current-wind');
+var currentHum = document.getElementById('#current-hum');
+var currentUv = document.getElementById('#current-uv');
+var tommorrowForecast = document.getElementById('#day-0');
+var tomorrowTemp = document.getElementById('#temp-0');
+var tomorrowWind = document.getElementById('#wind-0');
+var tomorrowHum = document.getElementById('#hum-0');
+var forecastDay2 = document.getElementById('#day-1');
+var day2Temp = document.getElementById('#temp-1');
+var day2Wind = document.getElementById('#wind-1');
+var day2Hum = document.getElementById('#hum-1');
+var forecastDay3 = document.getElementById('#day-2');
+var day3Temp = document.getElementById('#temp-2');
+var day3Wind = document.getElementById('#wind-2');
+var day3Hum = document.getElementById('#hum-2');
+var forecastDay4 = document.getElementById('#day-3');
+var day4Temp = document.getElementById('#temp-3');
+var day4Wind = document.getElementById('#wind-3');
+var day4Hum = document.getElementById('#hum-3');
+var forecastDay5 = document.getElementById('#day-4');
+var day5Temp = document.getElementById('#temp-4');
+var day5Wind = document.getElementById('#wind-4');
+var day5Hum = document.getElementById('#hum-4');
 
-var currentWeather = {
-    
-}
 
 var apiKey = 'a0261386ac4ee516e814165ba45ab66c';
 
+setInterval(() => {
+    const date = new Date();
+    const month = date.getMonth();
+    const day = date.getDay();
+    const year = date.getFullYear();
+
+    currentDate.innerHTML = '(' + month + '/' + day + '/' + year + ')';
+
+}, 1000);
